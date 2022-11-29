290a0a6586e99d0f6d4cc16af2430c3833a4b501
Mikhail Chatillon <mikhail.chatillon@hotmail.com>
2022-11-29 21:55:23 +0100

    last fix

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,8 @@
 import os
 import shutil
 local_dev = "{{ cookiecutter.local_dev }}"
-print(local_dev)
-if not local_dev:
+
+if local_dev == "False":
 
     os.remove('makefile')
     shutil.rmtree('docs/makefile')
